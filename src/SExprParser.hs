3d4054e033c442cfb58bd1ea14d94ac11d716214
Ryan <ryan.hercule@epitech.eu>
2024-11-19 05:15:53 -0500

    feat: parse list data type of LISP

--- src/SExprParser.hs
+++ src/SExprParser.hs
@@ -6,39 +6,44 @@
 -}
 
 module SExprParser
-    ( parseSExpr
+    ( parseSExpr,
+    parseSExprList
     ) where
 
 import Parser
 import Control.Applicative
 
-data SExpr = SExprAtomInt Int | SExprAtomString String | SExprString String | SExprList [SExpr] deriving (Show)
+data SExpr = SExprAtomInt Int | SExprAtomString String | SExprList [SExpr] deriving (Show)
 
 parseSExprAtomInt :: Parser SExpr
 parseSExprAtomInt = Parser fc where
     fc str = case runParser (parseMany (parseChar ' ')) str of
-        Right (_, rest) -> case runParser (parseUInt <* parseNotTheseChars ['a'..'z']) rest of
-            Right (num, rest1) -> Right (SExprAtomInt num, rest1)
+        Right (_, rest) -> case runParser parseUInt rest of
+            Right (num, rest1) -> 
+                case runParser (parseAnyChar ['a'..'z'] <|> parseAnyChar ['A'..'Z']) rest1 of
+                    Right (_, _) -> Left "Fail"
+                    Left _ -> Right (SExprAtomInt num, rest1)
             Left _ -> Left "Fail"
-        Left _  -> Left "Fail"
+        Left _ -> Left "Fail"
 
 parseSExprAtomString :: Parser SExpr
 parseSExprAtomString = Parser fc where
     fc str = case runParser (parseMany (parseChar ' ')) str of
-        Right (_, rest) -> case runParser (parseMany (parseNotThisChar ' ')) rest of
+        Right (_, rest) -> case runParser (parseMany (parseNotTheseChars " )")) rest of
             Right (str, rest1) -> Right (SExprAtomString str, rest1)
             Left _ -> Left "Fail"
         Left _  -> Left "Fail"
 
-parseSExprString :: Parser SExpr
-parseSExprString = Parser fc where
-    fc str = case runParser (parseMany (parseChar ' ')) str of
-        Right (_, rest) -> case runParser parseString rest of
-            Right (str, rest1) -> Right (SExprString str, rest1)
-            Left _ -> Left "Fail"
-        Left _  -> Left "Fail"
+parseSExprList :: Parser SExpr
+parseSExprList = parseChar '(' >> parseMany (parseAnyChar " \t\n") >>
+    ((:) <$> parseSExpr <*>
+    parseMany (parseAnyChar " \t\n" *>
+    parseMany (parseAnyChar " \t\n") *> parseSExpr)
+    <|> pure []) >>= \values ->
+    parseMany (parseAnyChar " \t\n")
+    >> parseChar ')' >> return (SExprList values)
 
 parseSExpr :: Parser SExpr
-parseSExpr = parseSExprAtomInt <|> parseSExprString <|> parseSExprAtomString
+parseSExpr = parseSExprList <|> parseSExprAtomInt <|> parseSExprAtomString
 
 
