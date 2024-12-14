50935cb82a2bcc32f42ea459a6c37153772c994f
Marius Rousset <marius.rousset@epitech.eu>
2024-12-14 18:26:16 +0100

    refactor: remove temporary types to only have one (prevent syntactic sugar code)

--- src/Token.hs
+++ src/Token.hs
@@ -5,45 +5,39 @@
 -- Token
 -}
 
-module Token (TokenType (..), Keyword (..), Symbol (..), Literal (..), Identifier (..)) where
+module Token (Token (..)) where
 
 -- \| SingleLineComment --    //
 -- \| MultiLineCommentOpen --    /*
 -- \| MultiLineCommentClose --    */
 
-data Keyword
-  = FnKw --       \| fn
+data Token
+  =
+  -- Keyword
+    FnKw --       \| fn
   | InfixKw --    \| infix
   | StructKw --   \| struct
   | IsKw --       \| is
   | ImportKw --   \| import
   | AsKw --       \| as
-  deriving (Show, Eq)
-
-data Symbol
-  = CurlyOpen --  \|  {   -> struct definition
+  | Symbol
+  | CurlyOpen --  \|  {   -> struct definition
   | CurlyClose -- \|  }   -> struct definition
   | ParenOpen --  \|  (   -> resolve expressions
   | ParenClose -- \|  )   -> resolve expressions
   | Assign --     \|  =   -> assign expression to a name (can be a func or a var)
   | Scope --      \|  .   -> used to import a module (std.print, math.facto, ...)
   | SemiColon --  \|  ;   -> end of statement
-  deriving (Show, Eq)
-
-data Literal
-  = IntLit Int --       \| 2
+  
+  -- Literal
+  | IntLit Int --       \| 2
   | FloatLit Double --  \| 1.5
   | StringLit String -- \| "yo"
-  deriving (Show, Eq)
-
-data Identifier
-  = PrefixId String --  \| factorial, add_2
+  
+  -- Identifier
+  | PrefixId String --  \| factorial, add_2
   | InfixId String  --  \| <*>, +
+
+  -- Type ? (int, float, string, char, number ?)
+
   deriving (Show, Eq)
-
-data TokenType = Kw Keyword | Sym Symbol | Lit Literal | Id Identifier deriving (Show, Eq)
-
--- test :: TokenType -> String
--- test (Lit x) = show x
--- test (Kw x) = show x
--- test (Sym x) = show x
