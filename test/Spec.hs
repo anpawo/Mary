7c4cdfd5bd469d431e46e90deb7024dc55011983
Marius Rousset <marius.rousset@epitech.eu>
2024-12-16 21:09:53 +0100

    feat(test): use hspec-discover to auto-generate the main

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,15 +1,1 @@
-{-
--- EPITECH PROJECT, 2024
--- glados
--- File description:
--- Spec
--}
-
-
-import TokenizerSpec (spec)
-
-import Test.Hspec (describe, hspec)
-
-main :: IO ()
-main = hspec $ do
-  describe "Tokenizer (Parser)" TokenizerSpec.spec
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
