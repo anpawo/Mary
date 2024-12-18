cdaf1647cc3a37553ad45681266f45261ba70820
Ryan <ryan.hercule@epitech.eu>
2024-12-18 06:24:28 -0500

    feat(token): comma token + split param in line by using comma

--- src/Parser/SortToken.hs
+++ src/Parser/SortToken.hs
@@ -7,8 +7,7 @@
 
 module Parser.SortToken
     (
-        splitBySemicolon,
-        sortToken
+      sortToken
     ) where
 
 
@@ -18,47 +17,59 @@ import Data.Functor (($>))
 import Control.Applicative ((<|>), some, empty)
 import Control.Monad (void)
 
-import Parser.SortTokenData
 import Parser.Token
 
-findLine :: [Token] -> [Token] -> ([SortToken], [Token])
-findLine [] [] = ([], [])
-findLine [] lines = ([Line (reverse lines)], [])
-findLine (CurlyClose:rest) [] = ([], CurlyClose:rest)
-findLine (CurlyClose:rest) lines = ([Line (reverse lines)], CurlyClose:rest)
-findLine (SemiColon:rest) lines = 
-  let (new_line, remaining) = findLine rest [] 
-  in (Line (reverse lines) : new_line, remaining)
-findLine (t:rest) lines = findLine rest (t:lines)
+findParams :: [Token] -> [Token] -> ([Token], [Token])
+findParams [] [] = ([], [])
+findParams [] linesList = ([Line (reverse linesList)], [])
+findParams (Comma:rest) linesList = 
+  let (new_line, remaining) = findParams rest [] 
+  in (Line (reverse linesList) : new_line, remaining)
+findParams (t:rest) linesList = findParams rest (t:linesList)
 
-splitBySemicolon :: [Token] -> ([SortToken], [Token])
+splitByComma :: [Token] -> ([Token], [Token])
+splitByComma [] = ([], [])
+splitByComma tokens = findParams tokens []
+
+findLine :: [Token] -> [Token] -> ([Token], [Token])
+findLine [] [] = ([], [])
+findLine [] linesList = ([Line (reverse linesList)], [])
+findLine (CurlyClose:rest) [] = ([], CurlyClose:rest)
+findLine (CurlyClose:rest) linesList = ([Line (reverse linesList)], CurlyClose:rest)
+findLine (SemiColon:rest) linesList = 
+  let (new_line, remaining) = findLine rest [] 
+  in (Line (reverse linesList) : new_line, remaining)
+findLine (t:rest) linesList = findLine rest (t:linesList)
+
+splitBySemicolon :: [Token] -> ([Token], [Token])
 splitBySemicolon [] = ([], [])
 splitBySemicolon tokens = findLine tokens []
 
-sortTokenStruct :: [Token] -> Either String [SortToken]
+sortTokenStruct :: [Token] -> Either String [Token]
 sortTokenStruct (SymbolId name: CurlyOpen : rest) =
   case span (/= CurlyClose) rest of
     (inside_struct, CurlyClose: rest_after_struct) -> do
-          let (lines, _) = splitBySemicolon inside_struct
+          let (linesList, _) = splitBySemicolon inside_struct
           restTokens <- sortToken rest_after_struct
-          Right (Structure {struct_name = name, struct_body = lines} : restTokens)
+          Right (StructureSort name linesList : restTokens)
     _ -> Left "error: invalid structure format"
 sortTokenStruct _ = Left "error: invalid struct format"
 
-sortTokenFunct :: [Token] -> Either String [SortToken]
+sortTokenFunct :: [Token] -> Either String [Token]
 sortTokenFunct (SymbolId name : ParenOpen : rest) =
   case span (/= ParenClose) rest of
     (params, ParenClose : Arrow : function_type :CurlyOpen : bodyTokens) ->
-      let (lines, rest_after_param) = splitBySemicolon bodyTokens
+      let (linesList, rest_after_param) = splitBySemicolon bodyTokens
       in if head rest_after_param == CurlyClose
          then do
+           let (sort_params, _) = splitByComma params
            restTokens <- sortToken (drop 1 rest_after_param)
-           Right (Function {func_name = name, func_param = params, func_type = function_type, func_body = lines} : restTokens)
+           Right (FunctionSort name sort_params function_type linesList : restTokens)
          else Left "error: missing or extra tokens after closing curly brace"
     _ -> Left "error: invalid function format"
 sortTokenFunct _ = Left "error: invalid function format"
 
-sortToken :: [Token] -> Either String [SortToken]
+sortToken :: [Token] -> Either String [Token]
 sortToken [] = Right []
 sortToken (FunctionKw : function) = sortTokenFunct function
 sortToken (StructKw : structure) = sortTokenStruct structure
--- src/Parser/Token.hs
+++ src/Parser/Token.hs
@@ -24,6 +24,7 @@ data Token
   | Assign --     \|  =   -> assign expression to a name (can be a func or a var)
   | Arrow --      \|  ->  -> return type of functions
   | Scope --      \|  .   -> used to import a module (std.print, math.facto, ...)
+  | Comma
   | SemiColon --  \|  ;   -> end of statement
   -- Type
   | CharT --  \| char
@@ -41,4 +42,8 @@ data Token
   -- Identifier
   | SymbolId String --  \| factorial, add_2, x
   | OperatorId String  --  \| <*>, +
+  -- Sort token
+  | Line [Token]
+  | FunctionSort {func_sort_name :: String, func_sort_param:: [Token], func_sort_type :: Token, func_sort_body :: [Token]}
+  | StructureSort {struct_sort_name :: String, struct_sort_body :: [Token]}
   deriving (Show, Eq)
