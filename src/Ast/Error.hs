87a913e5d2f0abb71f4567e3611daaa1d3cecf8c
Marius Rousset <marius.rousset@epitech.eu>
2024-12-22 05:09:59 +0100

    feat(error): add custom error message

--- src/Ast/Error.hs
+++ src/Ast/Error.hs
@@ -4,9 +4,8 @@
 -- File description:
 -- ErrorMessage
 -}
-{-# LANGUAGE RecordWildCards   #-}
 
-module Ast.Error (errNameTaken, errImpossibleCase, prettyPrintError, errExpectedType) where
+module Ast.Error (errNameTaken, errImpossibleCase, prettyPrintError, errExpectedType, errTopLevelDef) where
 
 import Text.Megaparsec.Error (ParseErrorBundle(..), ParseError(..), ErrorFancy(..))
 import Parser.Token
@@ -15,12 +14,15 @@ import Data.List.NonEmpty (NonEmpty(..))
 import Data.Set.Internal (elemAt)
 
 errNameTaken :: String -> String
-errNameTaken name = "name '" ++ (purple name) ++ "' already taken."
+errNameTaken name = "name '" ++ purple name ++ "' already taken."
 
 errExpectedType :: Bool -> String
 errExpectedType voidOk
-    | voidOk = "expected a type (including void)."
-    | otherwise = "expected a type (excluding void)."
+    | voidOk = "expected a " ++ purple "type" ++ " (including void)."
+    | otherwise = "expected a " ++ purple "type" ++ " (excluding void)."
+
+errTopLevelDef :: String
+errTopLevelDef = "top level declaration must be " ++ purple "function" ++ ", " ++ purple "operator" ++ " or " ++ purple "struct."
 
 errImpossibleCase :: String
 errImpossibleCase = "Impossible case."
@@ -29,27 +31,23 @@ prettyPrintError :: [MyToken] -> ParseErrorBundle [MyToken] Void -> String
 prettyPrintError tokens (ParseErrorBundle {bundleErrors = errors, bundlePosState = _}) =
     case errors of
         (FancyError pos fancySet :| _) ->
-            case (0 `elemAt` fancySet) of
-                (ErrorFail err) -> " |\n | " ++ tokCons ++ (red tokErr) ++ tokLeft ++ "\n |" ++ (red pointer) ++ "\n" ++ (red "error") ++ ": " ++ err
+            case 0 `elemAt` fancySet of
+                (ErrorFail err) -> " |\n | " ++ tokCons ++ red tokErr ++ tokLeft ++ "\n |" ++ red pointer ++ "\n" ++ red "error" ++ ": " ++ err
                 x -> "This error should be transformed into a custom one:\n" ++ show x
             where
-                tokCons = unwords $ show <$> tc
-                -- tokCons = unwords $ elAt (pos - 3) ++ elAt (pos - 2) ++ elAt (pos - 1)
-                    where
-                        tc = take (pos - 1) tokens
-                --         elAt i = case tc !? i of
-                --             Nothing -> []
-                --             Just x -> [show x]
-                --         (!?) :: [a] -> Int -> Maybe a
-                --         xs !? n
-                --             | n < 0     = Nothing
-                --             | otherwise = foldr (\x r k -> case k of
-                --                                             0 -> Just x
-                --                                             _ -> r (k-1)) (const Nothing) xs n
-                tokErr  = " " ++ show (tokens !! (pos - 1)) ++ " "
+                tokCons = unwords $ show <$> suffix (take (pos - 1) tokens)
+                tokErr
+                    | pos > 0 = " " ++ show (tokens !! (pos - 1)) ++ " "
+                    | otherwise = ""
                 tokLeft = let tc = take 3 (drop pos tokens) in unwords $ show <$> tc
-                pointer = replicate (length tokCons + 2) ' ' ++ (replicate (length tokErr - 2) '^')
+                pointer = replicate (length tokCons + 2) ' ' ++ replicate (length tokErr - 2) '^'
         x -> "This error should be transformed into a custom one" ++ show x
+
+suffix :: [a] -> [a]
+suffix l
+    | len < 10 = l
+    | otherwise = drop (len - 10) l
+    where len = length l
 
 red :: String -> String
 red s = "\ESC[91m" ++ s ++ reset
