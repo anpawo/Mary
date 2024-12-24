63505bb54f73f7a049ee96057d7a982fbab6cc55
Marius Rousset <marius.rousset@epitech.eu>
2024-12-24 05:21:04 +0100

    feat(ast): function + return: almost done. missing error detail

--- src/Ast/Ast.hs
+++ src/Ast/Ast.hs
@@ -8,6 +8,9 @@
 {-# OPTIONS_GHC -Wno-partial-fields #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use lambda-case" #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# HLINT ignore "Use isNothing" #-}
 
 module Ast.Ast
   (
@@ -20,9 +23,9 @@ module Ast.Ast
     function
   ) where
 
--- import Debug.Trace (trace)
+import Debug.Trace (trace)
 
-import Text.Megaparsec (Parsec, single, eof, satisfy, choice, runParser, MonadParsec(..), setOffset, getOffset, optional)
+import Text.Megaparsec (Parsec, single, eof, satisfy, choice, runParser, MonadParsec(..), setOffset, getOffset, optional, someTill)
 import Data.Void (Void)
 import Data.Functor (($>))
 import Data.List (find)
@@ -36,9 +39,10 @@ type Parser = Parsec Void [MyToken]
 
 data SubExpression
   = VariableCall { varName :: String }
-  | FunctionCall { fnName :: String, fnArgs :: [SubExpression] }
+  | FunctionCall { fnName :: String, fnArgs :: [SubExpression]}
+  | OperatorCall { opName :: String, opArgs :: [SubExpression]}
   | Literal Literal
-  | Builtin { builtinName :: String } -- operator + - * /
+  | Builtin { builtinName :: String } -- operator + - * / -- this should only be set my default at the start of the parsing and handled by the vm
   deriving (Show, Eq)
 
 data Expression
@@ -60,6 +64,7 @@ builtin :: [Ast]
 builtin =
     [ Operator {opName = "+", opPrecedence = 6, opRetType = IntType, opArgLeft = (IntType, "l"), opArgRight = (IntType, "r"), opBody = [SubExpression $ Builtin "+"]}
     , Operator {opName = "*", opPrecedence = 7, opRetType = IntType, opArgLeft = (IntType, "l"), opArgRight = (IntType, "r"), opBody = [SubExpression $ Builtin "*"]}
+    , Function {fnName = "print", fnArgs = [], fnRetType = VoidType, fnBody = [SubExpression $ Builtin "print"]} -- todo: im not really sure about this one (maybe we need the any type)
     ]
 
 tokenToAst :: Parser Ctx
@@ -128,7 +133,6 @@ getNames (Structure {structName = name}:rst) = name : getNames rst
 getNames (Function {fnName = name}:rst) = name : getNames rst
 getNames (Operator {opName = name}:rst) = name : getNames rst
 
--- type Variable = [(Type, String)]
 type LocalVariable = [(Type, String)]
 type RetType = Type
 
@@ -162,8 +166,102 @@ exprVariable _ _ _ = failN $ errTodo "variable creation expression"
 exprSubexpr :: Ctx -> LocalVariable -> RetType -> Parser Expression
 exprSubexpr _ _ _ = failN $ errTodo "sub expression"
 
+data Group = GGr [Group] | GLit Parser.Token.Literal | GFn String [Group] | GVar String | GOp String [Group] deriving (Show) -- should contain an index to make better errors
+
+getGroup :: Parser Group
+getGroup = choice
+  [ eof *> fail errEndSubexpr
+  , GGr <$> (tok ParenOpen *> someTill getGroup (tok ParenClose) <|> failN errEmptyGroup)
+  , GLit <$> glit
+  , GVar <$> try gsym <* notFollowedBy (tok ParenOpen)
+  , GFn <$> gsym <*> (tok ParenOpen *> (tok ParenClose $> [] <|> getAllArgs))
+  , GOp <$> gop <*> pure []
+  ]
+  where
+    glit = satisfy (\case
+      Parser.Token.Literal _ -> True
+      _ -> False) >>= (\case
+          (Parser.Token.Literal x) -> pure x
+          _ -> failN errImpossibleCase)
+    gsym = satisfy (\case
+      Parser.Token.Identifier (SymbolId _) -> True
+      _ -> False) >>= (\case
+          (Parser.Token.Identifier (SymbolId x)) -> pure x
+          _ -> failN errImpossibleCase)
+    gop = satisfy (\case
+      Parser.Token.Identifier (OperatorId _) -> True
+      _ -> False) >>= (\case
+          (Parser.Token.Identifier (OperatorId x)) -> pure x
+          _ -> failN errImpossibleCase)
+    getAllArgs = do
+      arg <- getGroup
+      endFound <- tok ParenClose <|> tok Comma
+      case endFound of
+        ParenClose -> pure [arg]
+        Comma -> (arg :) <$> getAllArgs
+        _ -> failN errImpossibleCase
+
+type Idx = Int
+type Prec = Int
+type Name = String
+
+mountGroup :: Ctx -> [Group] -> Parser Group
+mountGroup _ [] = fail errEmptyExpr -- should never happen
+mountGroup _ [x@(GLit {})] = pure x
+mountGroup _ [x@(GFn {})] = pure x
+mountGroup _ [x@(GVar {})] = pure x
+mountGroup _ [x@(GOp _ [_, _])] = pure x
+mountGroup ctx (GGr group: rst) = mountGroup ctx group >>= mountGroup ctx . (:rst)
+mountGroup ctx group = do
+  trace ("group=" ++ show group) (pure ())
+  opId <- calcPrec Nothing (-1) 1 group
+  trace ("opid=" ++ show opId) (pure ())
+  case opId of
+    Nothing -> fail errImpossibleCase
+    (Just (x, name))
+      | x <= 0 -> fail $ errMissingOperand "left" name
+      | x >= length group - 1 -> fail $ errMissingOperand "right" name
+      | otherwise -> let l = group !! (x - 1)
+                         r = group !! (x + 1) in
+                        case l of
+                          (GOp _ []) -> fail $ errMissingOperand "left" name
+                          _ -> case r of
+                            (GOp _ []) -> fail $ errMissingOperand "right" name
+                            _ -> mountGroup ctx (take (x - 1) group ++ (GOp (getOpName (group !! x)) [l, r] : drop (x + 2) group))
+  where
+    calcPrec :: Maybe (Prec, Name) -> Idx -> Idx -> [Group] -> Parser (Maybe (Idx, Name))
+    calcPrec Nothing _ _ [] = pure Nothing
+    calcPrec (Just (_, name)) idx _ [] = pure $ Just (idx, name)
+    calcPrec prec idx idx2 (GOp name []: rst) = case find (isSame name) ctx of
+      Nothing -> fail $ errOpNotDefined name
+      Just (Operator {..}) -> case prec of
+        Nothing -> calcPrec (Just (opPrecedence, opName)) (idx + idx2) 1 rst
+        Just (oldPrec, _)
+          | oldPrec < opPrecedence -> calcPrec (Just (opPrecedence, opName)) (idx + idx2) 1 rst
+          | otherwise -> calcPrec prec idx (idx2 + 1) rst
+      Just _ -> fail errImpossibleCase
+    calcPrec prec idx idx2 (_: rst) = calcPrec prec idx (idx2 + 1) rst
+
+    isSame name (Operator {..}) = name == opName
+    isSame _ _ = False
+
+getOpName :: Group -> String
+getOpName (GOp name _) = name
+getOpName _ = error "error: the token isn't an operator"
+
+validateSubexpr :: Ctx -> LocalVariable -> SubExpression -> Parser ()
+validateSubexpr ctx locVar subex = failN $ errTodo "validate subexpr"
+
+toSubexpr :: Group -> Parser SubExpression
+toSubexpr group = failN $ errTodo "validate to subexpr"
+
 subexpression :: Ctx -> LocalVariable -> Parser SubExpression
-subexpression _ _ = failN $ errTodo "subexpression"
+subexpression ctx locVar = do
+  group <- someTill getGroup (tok SemiColon) <|> failN errEmptyExpr
+  mount <- mountGroup ctx group
+  subex <- trace (show mount) (toSubexpr mount)
+  validateSubexpr ctx locVar subex
+  return subex
 
 getFnBody :: Ctx -> LocalVariable -> RetType -> Parser [Expression]
 getFnBody ctx locVar retT = do
