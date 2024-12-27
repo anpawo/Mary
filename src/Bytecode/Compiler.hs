4d24a6d9048bbf6dfdf750712fb3a052d502fd10
Ryan <ryan.hercule@epitech.eu>
2024-12-27 15:18:46 -0500

    feat(bytecode): conv ast function into list of instruction

--- src/Bytecode/Compiler.hs
+++ src/Bytecode/Compiler.hs
@@ -17,9 +17,11 @@
 
 module Bytecode.Compiler
   (
+    testCompiler
   ) where
 
 import Ast.Ast
+import Parser.Token (Literal(..), Type(..))
 
 data Instruction
   = Push Value
@@ -27,11 +29,86 @@ data Instruction
   | Ret
   | Store String
   | Load String
+  | PushEnv String
+  deriving Show
 
 data Value
-  = VmInt Int
+  = VmChar Char
   | VmBool Bool
-  | VmStr String
+  | VmInt Int
+  | VmFloat Double
+  | VmString String
+  deriving Show
 
+data EnvVar = EnvVar { envVarName :: String, envVarBody :: [Instruction]} deriving Show
 
-data EnvVar = EnvVar { envVarName :: String, envVarBody :: [Instruction]}
+convertLiteral :: Literal -> Value
+convertLiteral (CharLit c) = VmChar c
+convertLiteral (BoolLit b) = VmBool b
+convertLiteral (IntLit i) = VmInt i
+convertLiteral (FloatLit f) = VmFloat f
+convertLiteral (StringLit s) = VmString s
+
+compileSubExpression :: SubExpression -> [Instruction]
+compileSubExpression (VariableCall varName) = [Load varName]
+compileSubExpression (FunctionCall fnName args) = concatMap compileSubExpression args ++ [PushEnv fnName, Call]
+compileSubExpression (Literal lit) = [Push (convertLiteral lit)]
+
+compileExpression :: Expression -> [Instruction]
+compileExpression (SubExpression subExpr) = compileSubExpression subExpr
+compileExpression (Variable (_, name) value) = compileSubExpression value ++ [Store name]
+compileExpression (Return value) = compileSubExpression value ++ [Ret]
+
+compileExpressions :: [Expression] -> [Instruction]
+compileExpressions = concatMap compileExpression
+
+compileParam :: (Type, String) -> [Instruction]
+compileParam (_, paramName) = [Store paramName]
+
+compileParams :: [(Type, String)] -> [Instruction]
+compileParams params = concatMap compileParam params
+
+astToEnvVar :: Ast -> Either String EnvVar
+astToEnvVar (Function fnName fnArgs _ fnBody) = Right $ EnvVar fnName (compileParams (reverse fnArgs) ++ compileExpressions fnBody)
+astToEnvVar other = Left $ "Unsupported AST to bytecode: " ++ show other
+
+compiler :: [Ast] -> Either String [EnvVar]
+compiler asts =
+  case mapM astToEnvVar asts of
+    Left err -> Left err
+    Right envVars -> Right envVars
+
+testCompiler :: IO ()
+testCompiler = do
+  let asts = [Function {
+      fnName = "add_mul",
+      fnArgs = [
+          (IntType, "a"),
+          (IntType, "b"),
+          (IntType, "c")
+      ],
+      fnRetType = IntType,
+      fnBody = [
+        Return {
+          retValue = FunctionCall {
+              fnName = "+",
+              fnArgs = [
+                  VariableCall {varName = "a"},
+                  FunctionCall {
+                      fnName = "*",
+                      fnArgs = [ VariableCall {varName = "b"}, VariableCall {varName = "c"} ]
+                  }
+              ]
+          }
+        }
+      ]
+    }, Function {
+      fnName = "main",
+      fnArgs = [],
+      fnRetType = IntType,
+      fnBody = [
+        SubExpression FunctionCall { fnName = "add_mul", fnArgs = [Literal (IntLit 1), Literal (IntLit 2), Literal (IntLit 3)]},
+        Return {retValue = Literal (IntLit 0)}
+      ]
+    }]
+  print $ compiler asts
