4980bc182b911a10bf96e6f0569759dd5965f9d6
camil <camil.kaddouri@epitech.eu>
2025-01-01 22:51:03 +0100

    feat(vm): virtualmachine test file renamed - Spec

---
+++ test/glados/VirtualMachineSpec.hs
@@ -0,0 +1,104 @@
+{-
+-- EPITECH PROJECT, 2024
+-- glados
+-- File description:
+-- Spec
+-}
+
+module VirtualMachineSpec (spec) where
+
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+import VM.VirtualMachine 
+  ( exec
+  , Value(..)
+  , Operator(..)
+  , Instruction(..)
+  , Program
+  , Env
+  , IntVal
+  , compile
+  )
+
+-- env
+testEnv :: Env
+testEnv =
+  [ ("fact", FuncVal
+      [ PushArg 0
+      , Push (IntVal 1)
+      , Push (OpVal Eq)
+      , Call
+      , JumpIfFalse 2
+      , Push (IntVal 1)
+      , Ret
+
+      , PushArg 0
+      , Push (IntVal 1)
+      , Push (OpVal Sub)
+      , Call
+      , PushEnv "fact"
+      , Call
+      , PushArg 0
+      , Push (OpVal Mul)
+      , Call
+      , Ret
+      ])
+  ]
+
+-- tests programs
+basicMathProgram :: Program
+basicMathProgram =
+  [ Push (IntVal 10)
+  , Push (IntVal 5)
+  , Push (OpVal Add)
+  , Call
+  , Ret
+  ]
+
+conditionalProgram :: Program
+conditionalProgram =
+  [ Push (IntVal 10)
+  , Push (IntVal 5)
+  , Push (OpVal Less)
+  , Call
+  , JumpIfFalse 2
+  , Push (IntVal 42)
+  , Ret
+  , Push (IntVal 0)
+  , Ret
+  ]
+
+factorialProgram :: Program
+factorialProgram =
+  [ Push (IntVal 5)
+  , PushEnv "fact"
+  , Call
+  , Ret
+  ]
+
+errorProgram :: Program
+errorProgram =
+  [ Push (IntVal 42)
+  , Push (OpVal Add)  -- stack underflow 
+  , Call
+  , Ret
+  ]
+
+spec :: Spec
+spec = describe "Virtual Machine Tests" $ do
+
+  it "1) executes basicMathProgram and returns 15" $ do
+    let result = exec [] [] basicMathProgram []
+    result `shouldBe` Right (IntVal 15)
+
+  it "2) executes conditionalProgram and returns 0" $ do
+    let result = exec [] [] conditionalProgram []
+    result `shouldBe` Right (IntVal 0)
+
+  it "3) executes factorialProgram and returns 120" $ do
+    let result = exec testEnv [] factorialProgram []
+    result `shouldBe` Right (IntVal 120)
+
+  it "4) executes errorProgram and fails with error message" $ do
+    let result = exec [] [] errorProgram []
+    result `shouldBe` Left "Add expects two IntVal on the stack"
