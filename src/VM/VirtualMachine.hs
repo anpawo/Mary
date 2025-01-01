148a7a83a308ad76d158bc16b9ad2eb42f70326d
camil <camil.kaddouri@epitech.eu>
2025-01-01 20:07:31 +0100

    feat(vm): implement Div operator

--- src/VM/VirtualMachine.hs
+++ src/VM/VirtualMachine.hs
@@ -30,6 +30,7 @@ exec (Push v : is) stack = exec is (v : stack)
 exec (Call Add : is) (IntVal a : IntVal b : stack) = exec is (IntVal (b + a) : stack)
 exec (Call Sub : is) (IntVal a : IntVal b : stack) = exec is (IntVal (b - a) : stack)
 exec (Call Mul : is) (IntVal a : IntVal b : stack) = exec is (IntVal (b * a) : stack)
+exec (Call Div : is) (IntVal a : IntVal b : stack)
   | a /= 0    = exec is (IntVal (b `div` a) : stack)
   | otherwise = error "Division by zero"
 exec _ _ = error "Invalid program"
