91986c8f8acf3e8bb879f8647602065541003892
camil <camil.kaddouri@epitech.eu>
2025-01-01 20:48:25 +0100

    feat(vm-test): add test environment for factorial

--- test/glados/VirtualMachineTest.hs
+++ test/glados/VirtualMachineTest.hs
@@ -8,3 +8,24 @@
 module Main where
 
 import VM.VirtualMachine
+
+-- env
+testEnv :: Env
+testEnv =
+  [ ("fact", FuncVal
+      [ PushArg 0
+      , Push (IntVal 1)
+      , Call Eq
+      , JumpIfFalse 2
+      , Push (IntVal 1)
+      , Ret
+      , PushArg 0
+      , Push (IntVal 1)
+      , Call Sub
+      , PushEnv "fact"
+      , Call
+      , PushArg 0
+      , Call Mul
+      , Ret
+      ])
+  ]
