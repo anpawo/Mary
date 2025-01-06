121d7580b5cbea1b427e057666e63d1889d536a4
Garois <jean-garice.denoncin@epitech.eu>
2025-01-06 14:51:34 +0100

    feat(AstSpec.h): coding style

--- test/unit-test/VirtualMachineSpec.hs
+++ test/unit-test/VirtualMachineSpec.hs
@@ -9,7 +9,7 @@ module VirtualMachineSpec (spec) where
 
 import Test.Hspec (Spec, describe, it, shouldBe)
 
-import VM.VirtualMachine 
+import VM.VirtualMachine
   ( exec
   , Value(..)
   , Operator(..)
@@ -77,7 +77,7 @@ factorialProgram =
 errorProgram :: Program
 errorProgram =
   [ Push (IntVal 42)
-  , Push (OpVal Add)  -- stack underflow 
+  , Push (OpVal Add)  -- stack underflow
   , Call
   , Ret
   ]
