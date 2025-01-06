179cb16bcf7b85eb9d3c74049943348da1266fa1
Marius Rousset <marius.rousset@epitech.eu>
2025-01-06 21:25:43 +0100

    feat(ast): split token parsers in TokenParser

---
+++ src/Ast/TokenParser.hs
@@ -0,0 +1,78 @@
+{-
+-- EPITECH PROJECT, 2025
+-- glados
+-- File description:
+-- TokenParser
+-}
+
+module Ast.TokenParser (operatorIdentifier, textIdentifier, constraintType, arrayType, structType, charType, boolType, nullType, intType, floatType, strType, voidType) where 
+
+import Parser.Token
+
+import Text.Megaparsec (Parsec, satisfy, single)
+
+import Data.Void (Void)
+
+
+type Parser = Parsec Void [MyToken]
+
+
+-- identifier check
+isSymIdentifier :: MyToken -> Bool
+isSymIdentifier (Identifier (TextId {})) = True
+isSymIdentifier _                        = False
+
+isOpIdentifier :: MyToken -> Bool
+isOpIdentifier (Identifier (OperatorId {})) = True
+isOpIdentifier _                            = False
+
+-- type check
+isConstraintType :: MyToken -> Bool
+isConstraintType (Type (ConstraintType {})) = True
+isConstraintType _                          = False
+
+isArrayType :: MyToken -> Bool
+isArrayType (Type (ArrType {})) = True
+isArrayType _                   = False
+
+isStructType :: MyToken -> Bool
+isStructType (Type (StructType {})) = True
+isStructType _                   = False
+
+-- identifier parser
+operatorIdentifier :: Parser String
+operatorIdentifier = opeName . identifier <$> satisfy isOpIdentifier
+
+textIdentifier :: Parser String
+textIdentifier = textName . identifier <$> satisfy isSymIdentifier
+
+-- type parser
+constraintType :: Parser Type
+constraintType = typing <$> satisfy isConstraintType
+
+arrayType :: Parser Type
+arrayType = typing <$> satisfy isArrayType
+
+structType :: Parser Type
+structType = typing <$> satisfy isStructType
+
+charType :: Parser Type
+charType = typing <$> single (Type CharType)
+
+boolType :: Parser Type
+boolType = typing <$> single (Type BoolType)
+
+nullType :: Parser Type
+nullType = typing <$> single (Type NullType)
+
+voidType :: Parser Type
+voidType = typing <$> single (Type VoidType)
+
+intType :: Parser Type
+intType = typing <$> single (Type IntType)
+
+floatType :: Parser Type
+floatType = typing <$> single (Type FloatType)
+
+strType :: Parser Type
+strType = typing <$> single (Type StrType)
