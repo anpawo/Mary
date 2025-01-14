e7021ddc682c10488adc5586977ee95cd9f560cf
camilepitech <camil.kaddouri@epitech.eu>
2025-01-14 21:56:02 +0100

    feat(VM-tests): bool operator exec tests

--- test/unit-test/VirtualMachineSpec.hs
+++ test/unit-test/VirtualMachineSpec.hs
@@ -10,92 +10,17 @@ module VirtualMachineSpec (spec) where
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 import VM.VirtualMachine
-  ( exec
-  , Program
-  -- , Env
-  )
-
-import Bytecode.Data
-
--- env
--- testEnv :: Env
--- testEnv =
---   [ ("fact", FuncVal
---       [ PushArg 0
---       , Push (IntVal 1)
---       , Push (OpVal Eq)
---       , Call
---       , JumpIfFalse 2
---       , Push (IntVal 1)
---       , Ret
-
---       , PushArg 0
---       , Push (IntVal 1)
---       , Push (OpVal Sub)
---       , Call
---       , PushEnv "fact"
---       , Call
---       , PushArg 0
---       , Push (OpVal Mul)
---       , Call
---       , Ret
---       ])
---   ]
-
--- tests programs
-basicMathProgram :: Program
-basicMathProgram =
-  [ Push (VmInt 10)
-  , Push (VmInt 5)
-  , Push (VmFunc "+")
-  , Call
-  , Ret
-  ]
-
--- conditionalProgram :: Program
--- conditionalProgram =
---   [ Push (IntVal 10)
---   , Push (IntVal 5)
---   , Push (OpVal Less)
---   , Call
---   , JumpIfFalse 2
---   , Push (IntVal 42)
---   , Ret
---   , Push (IntVal 0)
---   , Ret
---   ]
-
--- factorialProgram :: Program
--- factorialProgram =
---   [ Push (IntVal 5)
---   , PushEnv "fact"
---   , Call
---   , Ret
---   ]
-
--- errorProgram :: Program
--- errorProgram =
---   [ Push (IntVal 42)
---   , Push (OpVal Add)  -- stack underflow 
---   , Call
---   , Ret
---   ]
-
-spec :: Spec
-spec = describe "Virtual Machine Tests" $ do
-
-  it "executes basicMathProgram and returns 15" $ do
-    let result = exec [] basicMathProgram []
-    result `shouldBe` Right (VmInt 15)
-
-  -- it "executes conditionalProgram and returns 0" $ do
-  --   let result = exec [] [] conditionalProgram []
-  --   result `shouldBe` Right (IntVal 0)
-
-  -- it "executes factorialProgram and returns 120" $ do
-  --   let result = exec testEnv [] factorialProgram []
-  --   result `shouldBe` Right (IntVal 120)
-
-  -- it "executes errorProgram and fails with error message" $ do
-  --   let result = exec [] [] errorProgram []
-  --   result `shouldBe` Left "Add expects two IntVal on the stack"
+boolOperatorExecSpec :: Spec
+boolOperatorExecSpec = do
+  describe "boolOperatorExec" $ do
+    it "int < int" $ do
+      let stack = [VmInt 3, VmInt 10]
+      v <- boolOperatorExec "<" (<) 0 [] [] stack
+      v `shouldBe` VmBool False
+    it "float > float" $ do
+      let stack = [VmFloat 2.1, VmFloat 5.4]
+      v <- boolOperatorExec ">" (>) 0 [] [] stack
+      v `shouldBe` VmBool True
+    it "fail if different types" $ do
+      let stack = [VmInt 1, VmString "X"]
+      (boolOperatorExec "<" (<) 0 [] [] stack) `shouldThrow` anyException
