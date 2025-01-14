2b32681695403d9fd3be1a38654e88be25c82dc7
camilepitech <camil.kaddouri@epitech.eu>
2025-01-14 21:57:45 +0100

    feat(VM-tests): do current instruction test

--- test/unit-test/VirtualMachineSpec.hs
+++ test/unit-test/VirtualMachineSpec.hs
@@ -10,6 +10,39 @@ module VirtualMachineSpec (spec) where
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 import VM.VirtualMachine
+doCurrentInstrSpec :: Spec
+doCurrentInstrSpec = do
+  describe "doCurrentInstr" $ do
+    it "Ret with value" $ do
+      doCurrentInstr (Just Ret) 0 [] [] [VmInt 999] `shouldReturn` VmInt 999
+    it "Ret empty stack => fail" $ do
+      (doCurrentInstr (Just Ret) 0 [] [] []) `shouldThrow` anyException
+    it "Update => missing struct => fail" $ do
+      let prog = [Update "var"]
+      (exec 0 [] prog [VmInt 9, VmString "field"]) `shouldThrow` anyException
+    it "Store => new env variable" $ do
+      let prog = [Store "newVar"]
+      v <- exec 0 [] prog [VmInt 10]
+      v `shouldBe` VmInt 10
+    it "Load => loads function from env" $ do
+      let e = [("myFun",[Push (VmInt 77), Ret])]
+          prog = [Load "myFun"]
+      v <- exec 0 e prog []
+      v `shouldBe` VmInt 77
+    it "Load => fail not found" $ do
+      let prog = [Load "nope"]
+      (exec 0 [] prog []) `shouldThrow` anyException
+    it "Call => top of stack not VmFunc => fail" $ do
+      let prog = [Call]
+      (exec 0 [] prog [VmInt 5]) `shouldThrow` anyException
+    it "JumpBackward => negative index => fail or out of range" $ do
+      let prog = [JumpBackward 1, Push (VmInt 1)]
+      (exec 0 [] prog []) `shouldThrow` anyException
+    it "Nothing => end => returns top of stack" $ do
+      doCurrentInstr Nothing 0 [] [] [VmInt 10] `shouldReturn` VmInt 10
+    it "Nothing => empty stack => returns VmVoid" $ do
+      doCurrentInstr Nothing 0 [] [] [] `shouldReturn` VmVoid
+
 getcurrentInstrSpec :: Spec
 getcurrentInstrSpec = do
   describe "getcurrentInstr" $ do
