2cdb57a8c684c9928c9b3a82a1782a36136421d0
camilepitech <camil.kaddouri@epitech.eu>
2025-01-17 00:28:50 +0100

    feat(opti-tests): define functionSpec for testing unreachable function elimination

--- test/unit-test/OptimizerSpec.hs
+++ test/unit-test/OptimizerSpec.hs
@@ -21,3 +21,23 @@ spec = do
   reachableSpec
   defaultAstSpec
   optimizeExprPatternsSpec
+
+functionSpec :: Spec
+functionSpec = describe "eliminateUnused" $ do
+  it "removes functions that are not reachable" $ do
+    let asts =
+          [ Function { fnName = "hundred", fnArgs = [], fnRetType = IntType, fnBody = [Return (Lit (IntLit 100))] }
+          , Function { fnName = "thousand", fnArgs = [], fnRetType = IntType, fnBody = [Return (Lit (IntLit 1000))] }
+          , Function
+              { fnName = "main"
+              , fnArgs = []
+              , fnRetType = VoidType
+              , fnBody =
+                  [ Variable (IntType, "a") (FunctionCall "-" [ FunctionCall "+" [Lit (IntLit 5), Lit (IntLit 9)]
+                                                                  , FunctionCall "*" [Lit (IntLit 4), Lit (IntLit 5)]
+                                                                  ]
+                                      )
+                  , SubExpression (FunctionCall "print" [VariableCall "a"])
+                  ]
+              }
+          ]
