82c81f90ba8e0d1906d4c498cb6497b3dfca75a9
camilepitech <camil.kaddouri@epitech.eu>
2025-01-16 22:44:22 +0100

    feat(opti): recursive operation optimization

--- src/Opti/Optimizer.hs
+++ src/Opti/Optimizer.hs
@@ -1,56 +1,56 @@
-{- 
--- EPITECH PROJECT, 2025
+{-
+-- EPITECH PROJECT, 2024
 -- glados
 -- File description:
--- Optimizer
+-- Main
 -}
+
+{-# LANGUAGE LambdaCase #-}
 
 module Opti.Optimizer (optimizeAST) where
 
 import Ast.Ast
 import Parser.Token (Literal(..))
-import Debug.Trace (trace)
+import System.IO.Unsafe (unsafePerformIO)
 
 optimizeAST :: [Ast] -> [Ast]
 optimizeAST asts =
-  let _ = trace (show asts) ()
-      optimized = map optimizeAst asts
-      _ = trace (show optimized) ()
-  in optimized
+  unsafePerformIO $ do
+    putStrLn (show asts)
+    putStrLn ""
+    let optimized = map optimizeAst asts
+    putStrLn (show optimized)
+    putStrLn ""
+    return optimized
 
 optimizeAst :: Ast -> Ast
 optimizeAst fun@(Function { fnBody = body }) = fun { fnBody = map optimizeExpr body }
 optimizeAst other = other
 
 optimizeExpr :: Expression -> Expression
-optimizeExpr (SubExpression sub) = SubExpression (optimizeSubExpr sub)
-optimizeExpr (IfThenElse cond thenEx elseEx) =
-  IfThenElse (optimizeSubExpr cond) (map optimizeExpr thenEx) (map optimizeExpr elseEx)
-optimizeExpr (While cond body) =
-  While (optimizeSubExpr cond) (map optimizeExpr body)
-optimizeExpr (Variable meta val) = Variable meta (optimizeSubExpr val)
-optimizeExpr other = other
+optimizeExpr = \case
+  SubExpression sub -> SubExpression (fixOptSubExpr sub)
+  IfThenElse cond thenEx elseEx ->
+    IfThenElse (fixOptSubExpr cond) (map optimizeExpr thenEx) (map optimizeExpr elseEx)
+  While cond body ->
+    While (fixOptSubExpr cond) (map optimizeExpr body)
+  Variable meta val -> Variable meta (fixOptSubExpr val)
+  other -> other
+
+fixOptSubExpr :: SubExpression -> SubExpression
+fixOptSubExpr sub =
+  let sub' = optimizeSubExpr sub
+  in if sub' == sub then sub' else fixOptSubExpr sub'
 
 optimizeSubExpr :: SubExpression -> SubExpression
-optimizeSubExpr fc@(FunctionCall { fnCallName = "+", fnCallArgs = args }) =
-  case args of
-    [Lit (IntLit x), Lit (IntLit y)] -> Lit (IntLit (x + y))
-    _ -> fc { fnCallArgs = map optimizeSubExpr args }
-optimizeSubExpr fc@(FunctionCall { fnCallName = "-", fnCallArgs = args }) =
-  case args of
-    [Lit (IntLit x), Lit (IntLit y)] -> Lit (IntLit (x - y))
-    _ -> fc { fnCallArgs = map optimizeSubExpr args }
-optimizeSubExpr fc@(FunctionCall { fnCallName = "*", fnCallArgs = args }) =
-  case args of
-    [Lit (IntLit x), Lit (IntLit y)] -> Lit (IntLit (x * y))
-    _ -> fc { fnCallArgs = map optimizeSubExpr args }
-optimizeSubExpr fc@(FunctionCall { fnCallName = "/", fnCallArgs = args }) =
-  case args of
-    [Lit (IntLit x), Lit (IntLit y)] ->
-      if y == 0
-         then fc
-         else Lit (IntLit (x `div` y))
-    _ -> fc { fnCallArgs = map optimizeSubExpr args }
-optimizeSubExpr fc@(FunctionCall { fnCallArgs = args }) =
-  fc { fnCallArgs = map optimizeSubExpr args }
+optimizeSubExpr fc@(FunctionCall { fnCallName = op, fnCallArgs = args }) =
+  let newArgs = map fixOptSubExpr args
+      newNode = fc { fnCallArgs = newArgs }
+  in case (op, newArgs) of
+       ("+", [Lit (IntLit x), Lit (IntLit y)]) -> Lit (IntLit (x + y))
+       ("-", [Lit (IntLit x), Lit (IntLit y)]) -> Lit (IntLit (x - y))
+       ("*", [Lit (IntLit x), Lit (IntLit y)]) -> Lit (IntLit (x * y))
+       ("/", [Lit (IntLit x), Lit (IntLit y)]) ->
+         if y == 0 then newNode else Lit (IntLit (x `div` y))
+       _ -> newNode
 optimizeSubExpr other = other
