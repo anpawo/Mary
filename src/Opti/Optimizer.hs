4e6b0c7f615ef7910bba90b9b7e1038d09944594
camilepitech <camil.kaddouri@epitech.eu>
2025-01-16 22:57:57 +0100

    feat(opti): dead code elimination: removing uncalled declared function from AST

--- src/Opti/Optimizer.hs
+++ src/Opti/Optimizer.hs
@@ -12,6 +12,7 @@ module Opti.Optimizer (optimizeAST) where
 import Ast.Ast
 import Parser.Token (Literal(..))
 import System.IO.Unsafe (unsafePerformIO)
+import qualified Data.Set as Set
 
 optimizeAST :: [Ast] -> [Ast]
 optimizeAST asts =
@@ -19,12 +20,15 @@ optimizeAST asts =
     putStrLn (show asts)
     putStrLn ""
     let optimized = map optimizeAst asts
-    putStrLn (show optimized)
+        optimized' = map optimizeExprInAst optimized
+        pruned = eliminateUnused optimized'
+    putStrLn (show pruned)
     putStrLn ""
-    return optimized
+    return pruned
 
 optimizeAst :: Ast -> Ast
-optimizeAst fun@(Function { fnBody = body }) = fun { fnBody = map optimizeExpr body }
+optimizeAst fun@(Function { fnBody = body }) =
+  fun { fnBody = map optimizeExpr body }
 optimizeAst other = other
 
 optimizeExpr :: Expression -> Expression
@@ -54,3 +58,48 @@ optimizeSubExpr fc@(FunctionCall { fnCallName = op, fnCallArgs = args }) =
          if y == 0 then newNode else Lit (IntLit (x `div` y))
        _ -> newNode
 optimizeSubExpr other = other
+
+optimizeExprInAst :: Ast -> Ast
+optimizeExprInAst fun@(Function { fnBody = body }) =
+  fun { fnBody = map optimizeExpr body }
+optimizeExprInAst other = other
+
+collectCallsExpr :: Expression -> [String]
+collectCallsExpr (SubExpression sub) = collectCallsSubExpr sub
+collectCallsExpr (IfThenElse cond thenEx elseEx) =
+  collectCallsSubExpr cond ++ concatMap collectCallsExpr thenEx ++ concatMap collectCallsExpr elseEx
+collectCallsExpr (While cond body) =
+  collectCallsSubExpr cond ++ concatMap collectCallsExpr body
+collectCallsExpr (Variable _ val) = collectCallsSubExpr val
+collectCallsExpr _ = []
+
+collectCallsSubExpr :: SubExpression -> [String]
+collectCallsSubExpr (FunctionCall { fnCallName = name, fnCallArgs = args }) =
+  name : concatMap collectCallsSubExpr args
+collectCallsSubExpr (Lit _) = []
+collectCallsSubExpr (VariableCall name) = [name]
+  
+collectCallsAst :: Ast -> [String]
+collectCallsAst (Function { fnBody = body }) = concatMap collectCallsExpr body
+collectCallsAst _ = []
+
+reachableFunctions :: [Ast] -> Set.Set String
+reachableFunctions asts =
+  let _ = [ fnName f | f@(Function {}) <- asts ]
+      go :: Set.Set String -> Set.Set String
+      go acc =
+        let new = Set.fromList [ name
+                               | ast <- asts
+                               , let cs = collectCallsAst ast
+                               , name <- cs
+                               , not (Set.member name acc)
+                               ]
+        in if Set.null new then acc else go (Set.union acc new)
+  in go (Set.singleton "main")
+
+eliminateUnused :: [Ast] -> [Ast]
+eliminateUnused asts =
+  let reachable = reachableFunctions asts
+  in filter (\ast -> case ast of
+                       Function { fnName = name } -> Set.member name reachable
+                       _ -> True) asts
