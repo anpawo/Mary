9553434990ef425298c9bb2d26dbb1d36254f5db
Garice <jean-garice.denoncin@epitech.eu>
2025-01-17 10:32:29 +0100

    feat(main) remove warnings

--- app/Main.hs
+++ app/Main.hs
@@ -8,26 +8,10 @@
 module Main (main) where
 
 import System.Environment (getArgs)
-import System.Exit (exitWith, ExitCode(..), exitSuccess)
-import Control.Exception (IOException, catch)
-import Data.List (intercalate)
-import Data.Maybe (isNothing, fromJust)
-import Text.Megaparsec (errorBundlePretty)
+import Utils.ArgParser (parseArguments)
+import System.Exit (exitWith, ExitCode(..))
 
-import Opti.Optimizer (optimizeAST)
-import Utils.Lib (run)
-import Utils.ArgParser (parseArguments, Arguments (..), OutputType (..))
-import Parser.Tokenizer (tokenize)
-import Ast.Parser (tokenToAst)
-import Ast.Error (prettyPrintError, bgBlack, colorblindMode)
-import Bytecode.Compiler (compiler)
-import Bytecode.Display (displayBytecode)
-import VM.VirtualMachine (exec)
-import Ast.Import (resolveImports)
-import Control.Monad (void)
-import System.Directory (makeAbsolute)
-
-import Ast.HandleArg (handleArgs, helper, glados)
+import Ast.HandleArg (handleArgs, helper)
 
 main :: IO ()
 main = getArgs >>= \args -> either (\_ -> putStrLn helper >> exitWith (ExitFailure 1)) handleArgs (parseArguments args)
