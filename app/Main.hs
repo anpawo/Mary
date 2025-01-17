11cfedbc03c4646bf9d2128110eadb4d6a0296ff
Garice <jean-garice.denoncin@epitech.eu>
2025-01-17 10:18:59 +0100

    feat(main) remove handlearg from main

--- app/Main.hs
+++ app/Main.hs
@@ -27,60 +27,7 @@ import Ast.Import (resolveImports)
 import Control.Monad (void)
 import System.Directory (makeAbsolute)
 
-
-glados :: Arguments -> String ->  IO ()
-glados args = toToken
-    where
-        toToken input = case run tokenize input of
-            Left err -> putStrLn (errorBundlePretty err) >> exitWith (ExitFailure 1)
-            Right tokens
-                | tokenTy $ argOutputType args -> putStrLn $ intercalate "  " (map (bgBlack . show) (snd tokens))
-                | otherwise -> toAst tokens
-
-        toAst (pos, tokens) = do
-            (builtins, imports) <- resolveImports args tokens
-            case run (tokenToAst builtins imports) tokens of
-                Left err -> putStrLn ((if argColorblind args then colorblindMode else id) $ prettyPrintError (fromJust $ argInputFile args) pos tokens err) >> exitWith (ExitFailure 1)
-                Right ast -> do
-                    ast' <- if argOptimize args then optimizeAST ast else return ast
-                    if astTy (argOutputType args)
-                    then print ast'
-                    else toBytecode ast'
-
-        toBytecode ast = case compiler ast of
-            Left err -> print err >> exitWith (ExitFailure 1)
-            Right (instr, env)
-                | bytecodeTy $ argOutputType args -> displayBytecode instr env
-                | otherwise -> runVm env instr
-
-        runVm env instr = void $ exec 0 env instr []
-
-helper :: String
-helper =
-    "execute the .mary file.\n" ++
-    "\n" ++
-    "<file>             => mandatory: an input file.\n" ++
-    "\n" ++
-    "--token            => display the tokens.\n" ++
-    "--ast              => display the ast.\n" ++
-    "--bytecode         => display the bytecode.\n" ++
-    "--import <path>    => import path for the libraries.\n" ++
-    "--optimize         => optimize the code before running it.\n" ++
-    "--colorblind       => display colors according to most colorblinds.\n" ++
-    "--no-builtins      => doesn't load the builtins.\n"
-
-handleArgs :: Arguments -> IO ()
-handleArgs args
-    | argShowHelper args = putStrLn helper >> exitSuccess
-    | isNothing $ argInputFile args = putStrLn helper >> exitWith (ExitFailure 1)
-    | otherwise = getFile (fromJust $ argInputFile args) >>=
-        \input -> makeAbsolute (fromJust $ argInputFile args) >>=
-            \path -> glados args { argInputFile = Just path } input
-    where
-        getFile file = catch (readFile file) invalidFile
-
-        invalidFile :: IOException -> IO String
-        invalidFile _ = putStrLn "Invalid input file." >> exitWith (ExitFailure 1)
+import Ast.HandleArg (handleArgs, helper, glados)
 
 main :: IO ()
 main = getArgs >>= \args -> either (\_ -> putStrLn helper >> exitWith (ExitFailure 1)) handleArgs (parseArguments args)
